b3394cc18cfbfe33ed4c7b4c604b1fb39cf6ced7
xmg <5750529+xmgProject@user.noreply.gitee.com>
2020-07-12 11:36:41 +0800

    登录完成

---
+++ src/components/Login.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="logo" />
+      </div>
+      <!-- 登录表单 -->
+      <el-form ref="loginFormRef" :rules="loginFormRules" :model="loginForm" label-width="0px" class="login_form">
+        <el-form-item prop="username">
+          <el-input prefix-icon="iconfont icon-user" v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input prefix-icon="iconfont icon-3702mima" v-model="loginForm.password" type="password"></el-input>
+        </el-form-item>
+        <!-- 按钮区 -->
+        <el-form-item class="login_btn">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入登录密码', trigger: 'change' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    // 重置表单
+    resetLoginForm() {
+      this.loginForm.username = ''
+      this.loginForm.password = ''
+      this.$refs.loginFormRef.resetFields()
+    },
+    // 登录
+    login() {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) {
+          return
+        }
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status !== 200) {
+          return this.$message.error('登录失败')
+        } else {
+          this.$message.success('登录成功')
+          window.sessionStorage.setItem('token', res.token)
+          this.$router.push('/home')
+        }
+      })
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.login_container {
+  background: #2b4b6b;
+  height: 100%;
+}
+.login_box {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  width: 450px;
+  height: 300px;
+  background: #fff;
+  border-radius: 50px;
+  transform: translate(-50%, -50%);
+}
+.avatar_box {
+  position: absolute;
+  left: 50%;
+  width: 130px;
+  height: 130px;
+  padding: 10px;
+  overflow: hidden;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  box-shadow: 0 0 10px #ddd;
+  background-color: #ffff;
+  transform: translate(-50%, -50%);
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: #eee;
+  }
+}
+.login_btn {
+  display: flex;
+  justify-content: center;
+}
+.login_form {
+  position: absolute;
+  bottom: 0px;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+</style>
